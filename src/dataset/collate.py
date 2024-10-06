43a68a3c2c8654c401c12277cdd72d885879ddc4
stingrayShih <82072233+stingrayShih@users.noreply.github.com>
2024-10-05 20:28:00 -0400

    Update collate.py

--- src/dataset/collate.py
+++ src/dataset/collate.py
@@ -19,18 +19,26 @@ class DataCollator(DataCollatorForTokenClassification):
         # The updated type of the three variables should be `torch.int64``.
         # Hint: some functions and variables you may want to use: `self.tokenizer.pad()`, `self.label_pad_token_id`.
         # --- TODO: start of your code ---
+        
         batch = self.tokenizer.pad(
-            {"input_ids": tk_ids, "attention_mask": attn_masks, "labels": lbs},
+            {"input_ids": tk_ids, "attention_mask": attn_masks, 'labels':lbs},
             padding=True
         )
-        
-        
+
         tk_ids = torch.LongTensor(batch['input_ids'])
-        print("tk_ids",tk_ids.shape)
+        #print("tk_ids",tk_ids.shape, tk_ids.dtype)
+        
         attn_masks = torch.LongTensor(batch['attention_mask'])
-        print("attn_masks",attn_masks.shape)
-        print(batch['labels'])
-        lbs = torch.LongTensor(batch['labels'])
+        #print("attn_masks",attn_masks.shape, attn_masks.dtype)
+
+        max_len=tk_ids.shape[1]
+        #print('max_len',max_len)
+        for i in range(len(lbs)):
+          lbs[i] = lbs[i]+[self.label_pad_token_id]*(max_len-len(lbs[i]))
+          #print(lbs[i])
+
+        lbs=torch.LongTensor(lbs)
+        #print("lbs", lbs.shape, lbs.dtype)
 
         # --- TODO: end of your code ---
 
